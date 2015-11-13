aec938c6ace4fc5eaf1c1a5d1c8b5f4a5016c949
sevillaj <jls2rf@mail.umkc.edu>
2015-11-12 21:28:16 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,65 +1,49 @@
 #include "Library.h"
-//#include "queue.h"
-//#include <iostream>
 using namespace std;
 
 int main(){
-	int days_to_wait = 10;
-	Employee Employee1 = Employee("Bob");
-	/*cout << "Name: " << Employee1.get_employee() << endl
-		<< "Waiting time: " << Employee1.get_waiting() << endl
-		<< "Time to have book: " << Employee1.get_retaining() << endl << endl;*/
-	//cout << Employee1;
-	
 
-	/*string isArchived;
-	Book NewBook = Book("Lion King", Date(2015, 3, 1, DateFormat::US));
-	if (NewBook.get_archived() == true){
-		isArchived = "true";
-	}
-	else{
-		isArchived = "false";
-	}
-	*/
-	/*cout << "Title: " << NewBook.get_title() << endl
-		<< "Currently in Library: " << isArchived << endl
-		<< "Will begin circulating: " << NewBook.get_start() << endl << endl;*/
-	
-	Employee Employee2 = Employee("Sarah");
-	Employee Employee3 = Employee("Jake From State Farm");
-	//queue<Employee> Employee_Queue;
-	//Employee_Queue.push(Employee1);
-	//Employee_Queue.push(Employee2);
-	
-	/*NewBook.add_employee(Employee1);
-	NewBook.add_employee(Employee2);
-	NewBook.add_employee(Employee3);*/
-	//NewBook.print_waiting_list();
 
 	Library library;
+	
+	Employee Ann = Employee("Ann");
+	Employee Sam = Employee("Sam");
+	Employee Adam = Employee("Adam");
 
-	//Book Book2 = Book ("Book2", Date(2015, 3, 1, DateFormat::US));
-	//Book Book3 = Book("Book3", Date(2015, 3, 1, DateFormat::US));
-	Book Book2 = Book("Book2");
-	Book Book3 = Book("Book3");
+	Employee* Employee_P;
+	Employee_P = &Ann;
 
-	library.add_book(Book2);
-	//library.add_book(Book2);
-	//library.add_book(Book3);
-	//library.print_archived();
-	
-	library.add_employee(Employee1, Book2);
-	library.circulate_book(Book2, Date(2014, 3, 1, DateFormat::US));
-	
-	
-	library.add_employee(Employee2, Book2);
-	library.add_employee(Employee3, Book2);
-	library.circulate_book(Book2, Date(2014, 10, 1, DateFormat::US));
-	
-	library.add_book(Book2);
+	Book SE = Book("Software Engineering");
+	Book Chem = Book("Chemistry");
 
+	library.add_book(SE);
+
+	library.add_employee(Employee_P, SE);
+	Employee_P = &Sam;
+	library.add_employee(Employee_P, SE);
+	Employee_P = &Adam;
+	library.add_employee(Employee_P, SE);
+
+	library.circulate_book(SE, Date(2015, 3, 1, DateFormat::US)); // begin circulation
+	library.pass_on(SE, Date(2015, 3, 10, DateFormat::US));
+	library.pass_on(SE, Date(2015, 3, 12, DateFormat::US));
+	library.pass_on(SE, Date(2015, 3, 15, DateFormat::US));
+
+	library.add_book(Chem);
+	Employee_P = &Ann;
+	library.add_employee(Employee_P, Chem);
+	Employee_P = &Sam;
+	library.add_employee(Employee_P, Chem);
+	Employee_P = &Adam;
+	library.add_employee(Employee_P, Chem);
+	library.circulate_book(Chem, Date(2015, 3, 1, DateFormat::US)); // begin circulation
+	library.pass_on(Chem, Date(2015, 3, 10, DateFormat::US));
+	library.pass_on(Chem, Date(2015, 3, 12, DateFormat::US));
+	library.pass_on(Chem, Date(2015, 3, 15, DateFormat::US));
+
+	Employee_P = nullptr;
+	delete Employee_P;
 
 	system("pause");
-	//library.delete_all();
 	return 0;
 }
